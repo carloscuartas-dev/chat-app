75bdf9b543776cf91902917913f8555e9c4a1260
Carlos Cuartas <carloscuartasm@gmail.com>
2020-06-05 22:55:52 +0200

    Server modification

--- server/server.js
+++ server/server.js
@@ -1,15 +1,27 @@
 const express = require('express');
 const path = require('path');
+const http = require('http');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 const app = express();
-
-console.log(__dirname + '/../public');
-console.log(publicPath);
+const server = http.createServer(app);
+const io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+  console.log('New user connected');
+
+  socket.on('disconnect', () => {
+    console.log('User was disconnected');
+  });
+
+});
+
+
+
+server.listen(port, () => {
   console.log(`App listen on port ${port}`);
 })
